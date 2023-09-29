2cc2b53cf87ca9dd036909d9058345e4974e3405
Gokulkrishnan R <gokulkrishnan.r@gallabox.com>
2023-09-29 19:40:57 +0530

    v1.3.1

---
+++ check.js
@@ -0,0 +1,15 @@
+"<p>Unable to deliver message to the Recipient. Reasons can include:</p>
+<ol>
+  <li>The person you're trying to message doesn't have WhatsApp on their phone.</li>
+  <li>They haven't agreed to WhatsApp's new rules and privacy policy.</li>
+  <li>They're using an old version of WhatsApp, and they need to update to a newer one:</li>
+</ol>
+<ul>
+  <li>If they're on Android, they should have version 2.21.15.15 or higher.</li>
+  <li>For SMBA (Samsung's Android version), they need 2.21.15.15 or newer.</li>
+  <li>iOS users should be using version 2.21.170.4 or higher.</li>
+  <li>If they're using SMBI (WhatsApp for Business on iOS), they should have version 2.21.170.4 or newer.</li>
+  <li>For KaiOS, the required version is 2.2130.10 or higher.</li>
+  <li>If they're using WhatsApp Web, they should have version 2.2132.6 or newer.</li>
+</ul>
+<p>Make sure they meet these requirements for your message to go through.</p>  "
